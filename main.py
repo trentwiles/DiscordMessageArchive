e921c226cba9c04a84053360ca54b78be381a55f
trentwiles <wilest7@gmail.com>
2023-12-03 17:40:09 -0800

    complete collector

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ def getMessages(channel_id, limit, before):
         ts = msg["timestamp"]
         if msg["edited_timestamp"] != None:
             hasEdit = True
+            editTs = msg["edited_timestamp"]
         else:
             hasEdit = False
+            editTs = None
         author = {"username": msg["author"]["username"], "userID": msg["author"]["id"], "avatar_url": f"https://cdn.discordapp.com/avatars/{msg['author']['id']}/{msg['author']['avatar']}.webp?size=32", "nickname": msg["author"]["global_name"]}
+        edits = {"hasEdits": hasEdit, "edit_timestamp": editTs}
+        end_product = {"content": content, "id": msg_id, "timestamp": ts, "edits": edits, "author": author}
+        
+        holder.append(end_product)
