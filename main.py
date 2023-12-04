553e9e3fbce599048679f4b80963ebcf3a89a1c0
trentwiles <wilest7@gmail.com>
2023-12-03 17:37:06 -0800

    even more

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ def getToken():
     return open("token.txt").read()
 
 def getMessages(channel_id, limit, before):
+    holder = []
     limit = str(limit)
     channel_id = str(channel_id)
+
     if before != -1:
-        x = requests.get(url="https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}", headers= {"Authorization": getToken()})
+        x = requests.get(url=f"https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}", headers= {"Authorization": getToken()})
     else:
-        x = requests.get(url="https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}", headers= {"Authorization": getToken()})
-    print(x.json())
+        x = requests.get(url=f"https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}", headers= {"Authorization": getToken()})
+    
+    if x.status_code != 200:
+        return None
+
+    for msg in x.json():
+        msg_id = msg["id"]
+        content = msg["content"]
+        ts = msg["timestamp"]
+        if msg["edited_timestamp"] != None:
+            hasEdit = True
+        else:
+            hasEdit = False
+        author = {"username": msg["author"]["username"], "userID": msg["author"]["id"], "avatar_url": f"https://cdn.discordapp.com/avatars/{msg['author']['id']}/{msg['author']['avatar']}.webp?size=32", "nickname": msg["author"]["global_name"]}
---
+++ tester.py
@@ -0,0 +1,4 @@
+import main
+
+
+main.getMessages(497185813264859137, 50, -1)
