15c883c55995a8cf555983e9442b9bae10671231
trentwiles <wilest7@gmail.com>
2023-12-11 00:25:50 -0800

    basic sql, unprotected

---
+++ db.py
@@ -0,0 +1,30 @@
+import os
+from sqlalchemy import create_engine, text
+import json
+
+username = json.loads(open("config.json").read())["username"]
+password = json.loads(open("config.json").read())["password"]
+address = json.loads(open("config.json").read())["address"]
+database = json.loads(open("config.json").read())["db"]
+
+def connect():
+    engine = create_engine(f"cockroachdb://{username}:{password}@{address}/{database}?sslmode=verify-full")
+    return engine.connect()
+
+# db structure
+"""
+CREATE TABLE msg (
+  content TEXT,
+  image TEXT,
+  sent_by VARCHAR(255),
+  serverID VARCHAR(255),
+  messageID VARCHAR(255),
+  epoch INT
+); 
+"""
+
+def ins(content, image, sent_by, serverID, messageID, epoch):
+    res = connect().execute(text(f"INSERT INTO msg (content, image, sent_by, serverID, messageID, epoch) VALUES ('{content}', '{image}', '{sent_by}', '{serverID}', '{messageID}', {epoch});"))
+
+def deleteAll():
+    res = connect().execute(text("delete from msg where 0=0;"))
