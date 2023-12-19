4f0bc55e672c2e03251c5cb1c09aa4e389bafcdc
RiversideRocks <wilest7@gmail.com>
2023-12-19 12:09:10 -0500

    testing improved

--- tester.py
+++ tester.py
@@ -1,4 +1,8 @@
 import links
 import makeHTML
+import extract
 
-print(links.scanForLinks({'id': '485196500830519300', 'name': 'Aoun Gang'}, 2, 100))
+channel_database = links.scanServers(2)
+for channel in channel_database:
+    # format: {'id': '485196500830519300', 'name': 'Aoun Gang'}
+    extract.getLimitedMessages(channel["id"], -1, 2, 500)
